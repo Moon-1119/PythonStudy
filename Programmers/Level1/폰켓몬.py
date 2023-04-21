ba0d27b70e836b71ae292724bb77a02f7452020c
Moon-1119 <55127179+Moon-1119@users.noreply.github.com>
2023-04-21 15:59:14 +0900

    Delete Programmers/Level1 directory

--- Programmers/Level1/폰켓몬.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-def solution(nums):
-    getNum = len(nums) // 2
-    uniqueNums = np.unique(nums)
-    if len(uniqueNums) < getNum:
-        return len(uniqueNums)
-    else:
-        return(getNum)
